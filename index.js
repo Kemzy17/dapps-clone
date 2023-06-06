08183c7cd3b391aef7da624e931fa9d43f9a3470
kamisi pamilerin <kamisipamilerin@gmail.com>
2023-06-06 01:56:39 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var connectwallet = document.getElementById("connectwallet");
+function connect() {
+  document.getElementById("root").style.display = "none";
+  document.body.style.background = "#041938";
+  document.getElementById("connecting").style.display = "flex";
+}
